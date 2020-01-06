f0be4e3896a36f85813bc572566665fdf9cc9662
Brad Davies <varbrad@users.noreply.github.com>
2020-01-06 21:27:37 +0000

    Try and fix secret deployment

--- src/pages/secrets.tsx
+++ src/pages/secrets.tsx
@@ -1,6 +1,5 @@
-const appEnv = process.env.APP_ENV
-
 const Secrets = (): JSX.Element => {
+  const appEnv = process.env.APP_ENV
   if (!appEnv) return <p>No app env present!</p>
   return <p>App env = {appEnv}</p>
 }
